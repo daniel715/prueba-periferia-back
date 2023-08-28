12da43283529eecce04b0e750b74407a80775495
Daniel Montellanos <daniel.montellanos.contractor@bbva.com>
2023-08-28 13:00:59 -0500

    funcional hasta front, iniciando JWT implemntation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,3 +15,15 @@ CREATE TABLE Tasa(
 
  insert into Tasa (id, monedaOrigen,monedaDestino,tasa)values(5,'euro','soles', 3.99 );
  insert into Tasa (id, monedaOrigen,monedaDestino,tasa)values(6,'euro','dolar', 1.08 );
+
+select * from usuario;
+
+DROP TABLE IF EXISTS Usuario;
+CREATE TABLE Usuario(
+                     idusuario  int NOT NULL,
+                     nombre varchar(10) ,
+                     email varchar(50) ,
+                     password varchar(100),
+                     PRIMARY KEY (idusuario)
+);
+insert into Usuario (idusuario, nombre,email,password)values(1,'admin','admin', '$2a$10$RXWPzQOx4jbgSyUCiWQpTOwlqzFmcj9CnJF8wwJnR8loxe4om42Aq' );
